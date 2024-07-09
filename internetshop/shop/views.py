26736ebca619f19f1a66262effe3fb1e63de4706
Your Name <you@example.com>
2024-07-09 19:17:54 +0300

    Добавил бота

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Product, Review
+import telebot
 
+from .config import BOT_TOKEN, CHAT_ID
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 # Create your views here.
 
@@ -46,4 +51,14 @@ def view_product(request, id):
     })
 
 def payment(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        # Send message to Telegram
+        bot.send_message(CHAT_ID, f'''📦 Новый заказ: Тест
+
+ФИО покупателя: {name}
+Адрес доставки: {address}
+''')
+
     return render(request, "payment.html")
