91ae44b0c73f96b21555f89f54ad4ea0f065cfe9
Your Name <you@example.com>
2024-07-10 19:08:53 +0300

    Убрал DEBUG

--- internetshop/internetshop/settings.py
+++ internetshop/internetshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#a(eb0zz8rvf&6g-s0b3o5!j#2$=zsb$8=je71xf-%)*-s%)o6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
